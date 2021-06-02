51dc411f2db7d27da6f251b198d18b17872d8243
Osaki <c200023@ccg.ac.jp>
2021-06-02 10:49:25 +0000

    lec-02

---
+++ lec-02/calc.php
@@ -0,0 +1,9 @@
+<?php
+echo "国語のテスト：80点<br>";
+echo "数学のテスト：75点<br>";
+echo "英語のテスト：80点<br>";
+$total = 80 + 75 + 80;
+$average = $total / 3;
+echo "合計点：{$total}点<br>";
+echo "平均点：{$average}点<br>";
+?>
